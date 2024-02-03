434d3e02674258cff7afe75b4405017dba2e9215
RogueSMG <gothi.satyam1@gmail.com>
2024-02-03 21:21:27 +0000

    updated code

--- hypervisor.py
+++ hypervisor.py
@@ -4,8 +4,38 @@ import telebot
 import base64
 import shell
 import config
+# from shell import shell as shell1
 
+# shell1 = shell.shell
 bot = telebot.TeleBot(config.token)
+
+
+
+@bot.message_handler(commands=['spawn'], func=lambda message: message.chat.type == "private")
+def send_spawn_command_result(message):
+    if message.from_user.id == 673748261:
+        # bot.send_message(message.chat.id, "in")
+        try:
+            pwfile = open(config.password_file, "r")
+            pwfile_content = pwfile.read().strip()
+            # bot.send_message(message.chat.id, pw)
+            if pwfile_content is None or pwfile_content != "":
+                # bot.send_message(message.chat.id, "aagad")
+                pw = base64.urlsafe_b64decode(pwfile_content.encode('utf-8')).decode('utf-8')
+                # bot.send_message(message.chat.id, message.text)
+                # bot.send_message(message.chat.id, shell.execute_sudo((message.text), pw), parse_mode="Markdown")
+                # command_to_run = "tmux new-session -d -s batman " + f"'{shell.split_command(message.text)}" + "; tmux wait-for -S done \; wait-for done \; capture-pane -p -t batman"
+                command_to_run = "/usr/bin/tmux new-session -d -s batman " + f"'{shell.split_command(message.text)}" + " ; /usr/bin/tmux wait-for -S done '\; wait-for done \; capture-pane -p -t batman"
+                # bot.send_message(message.chat.id, command_to_run)
+                bot.send_message(message.chat.id, "```\n" + shell.execute_sudo(command_to_run, pw) + "\n```", parse_mode="Markdown")
+        except Exception as e:
+            bot.send_message(message.chat.id, e)
+    else:
+        bot.send_message(message.chat.id, "Error: Permission denied.")
+
+
+
+
 
 
 @bot.message_handler(commands=['run'], func=lambda message: message.chat.type == "private")
@@ -17,14 +47,19 @@ def send_command_result(message):
         bot.send_message(message.chat.id, "Error: Permission denied.")
 
 
-@bot.message_handler(commands=['su_run'], func=lambda message: message.chat.type == "private")
+@bot.message_handler(commands=['su'], func=lambda message: message.chat.type == "private")
 def send_su_command_result(message):
-    if message.from_user.id in config.admins:
+    if message.from_user.id == 673748261:
+        # bot.send_message(message.chat.id, "in")
         try:
             pwfile = open(config.password_file, "r")
-            pwfile_content = pwfile.read()
+            pwfile_content = pwfile.read().strip()
+            # bot.send_message(message.chat.id, pw)
             if pwfile_content is None or pwfile_content != "":
+                # bot.send_message(message.chat.id, "aagad")
                 pw = base64.urlsafe_b64decode(pwfile_content.encode('utf-8')).decode('utf-8')
+                # bot.send_message(message.chat.id, message.text)
+                # bot.send_message(message.chat.id, shell.execute_sudo((message.text), pw), parse_mode="Markdown")
                 bot.send_message(message.chat.id, "```\n" + shell.execute_sudo(
                     shell.split_command(message.text), pw) + "\n```", parse_mode="Markdown")
         except Exception as e:
--- shell.py
+++ shell.py
@@ -5,12 +5,15 @@ import config
 
 def execute_one_line(command):
     if command is not None:
-        if int(os.getegid()) is 0 and config.normal_user != "":
-            command = "su " + config.normal_user + " -c " + command
+        #if int(os.getegid()) == 0 and config.normal_user != "":
+            # print(config.normal_user)
+            # command = "su -c " + command
+            # print("hi")
         if "exit" in command:
             return "Error: Unfortunately I do not accept exit"
         command += "; exit 0"
-        result = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True).decode('utf-8')
+        result = subprocess.check_output(command, shell=True).decode('utf-8')
+        # print(command)
         real_result = ''
         if "[sudo] password for" in result:
             results = result.split(': ')
@@ -18,8 +21,10 @@ def execute_one_line(command):
                 for arg in results:
                     if results.index(arg) > 0:
                         real_result += arg + ""
+        # print(real_result)
         else:
             real_result = result
+        # print(real_result)
         return real_result.strip()
     else:
         return "Error: Command is null"
@@ -27,7 +32,10 @@ def execute_one_line(command):
 
 def execute_sudo(command, passwd):
     if command is not None:
-        return execute_one_line("echo \"" + passwd + "\" | sudo -S " + command)
+        # print(config.normal_user)
+        # print(command)
+        # print(execute_one_line("echo '" + passwd + "' | sudo -c "  + command))
+        return execute_one_line("echo '" + passwd + "' | sudo "  + command)
     else:
         return "Error: Command is null"
 
@@ -38,4 +46,5 @@ def split_command(message):
     for arg in commands:
         if commands.index(arg) > 0:
             args += arg + " "
+    # print(args.strip())
     return args.strip()
